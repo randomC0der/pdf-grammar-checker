151b0385c05073bf21cf24e4958a26c19da1bd9f
Lukas Hein <lukashein@protonmail.com>
2020-12-12 13:51:13 +0100

    Add American language an ngram support

--- src/main/java/de/lukashein/pdf_grammar_checker/Main.java
+++ src/main/java/de/lukashein/pdf_grammar_checker/Main.java
@@ -7,8 +7,8 @@ import java.util.concurrent.Callable;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tika.exception.TikaException;
 import org.languagetool.JLanguageTool;
+import org.languagetool.language.AmericanEnglish;
 import org.languagetool.language.GermanyGerman;
 import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
@@ -16,10 +16,14 @@ import org.languagetool.rules.spelling.SpellingCheckRule;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
 @Slf4j
 public class Main implements Callable<Integer> {
 
+    @Option(names = "--ngram", description = "Directory to statistical ngram data")
     private File ngramDir;
+
+    private JLanguageTool langTool;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Main()).execute(args);
@@ -29,13 +33,32 @@ public class Main implements Callable<Integer> {
     @Override
     public Integer call() {
         try {
+
+            loadLanguage();
+
             if (ngramDir != null) {
+                langTool.activateLanguageModelRules(ngramDir);
             }
 
             return 0;
 
         } catch (Exception e) {
             return -1;
+        }
+    }
+
+    private void loadLanguage() {
+        switch (language) {
+            case DEUTSCH:
+                langTool = new JLanguageTool(new GermanyGerman());
+                break;
+
+            case ENGLISH:
+                langTool = new JLanguageTool(new AmericanEnglish());
+                break;
+
+            default:
+                throw new IllegalArgumentException();
         }
     }
 }
