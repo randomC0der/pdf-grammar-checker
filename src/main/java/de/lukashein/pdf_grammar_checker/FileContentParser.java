13df98d6c4dd91aa816d8cdf21a632775394b32a
Lukas Hein <lukashein@protonmail.com>
2020-12-12 03:46:44 +0100

    Read txt files

--- src/main/java/de/lukashein/pdf_grammar_checker/FileContentParser.java
+++ src/main/java/de/lukashein/pdf_grammar_checker/FileContentParser.java
@@ -1,6 +1,7 @@
 package de.lukashein.pdf_grammar_checker;
 
 import lombok.NonNull;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
@@ -10,6 +11,7 @@ import org.apache.tika.metadata.Metadata;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public abstract class FileContentParser {
     private final static Tika tika = new Tika();
@@ -23,18 +25,29 @@ public abstract class FileContentParser {
 
     /**
      *
-     * @param pdfDoc The file to be parsed
+     * @param file The file to be parsed
      * @return The formatted file content
      * @throws IOException if the document can not be read
      * @throws TikaException if the document can not be parsed
      */
-    public String parse(@NonNull File pdfDoc) throws IOException, TikaException {
-        if(!FilenameUtils.getExtension(pdfDoc.getName()).equalsIgnoreCase("pdf")) {
-            throw new IOException("Unsupported file extension");
+    public String parse(@NonNull File file) throws IOException, TikaException {
+        String fileContent;
+
+        switch (FilenameUtils.getExtension(file.getName())) {
+            case "pdf":
+                final Metadata metadata = new Metadata();
+                final InputStream stream = TikaInputStream.get(file.toPath(), metadata);
+                fileContent = tika.parseToString(stream, metadata, 250_000);
+                break;
+
+            case "txt":
+                fileContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+                break;
+
+            default:
+                throw new IOException("Unsupported file extension");
         }
 
-        final Metadata metadata = new Metadata();
-        final InputStream stream = TikaInputStream.get(pdfDoc.toPath(), metadata);
-        return format(tika.parseToString(stream, metadata, 250_000));
+        return format(fileContent);
     }
 }
